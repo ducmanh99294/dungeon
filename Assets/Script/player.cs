0b7b23a9bfaf2d67bc2b78c8484aa1d6f354a275
manh <manh99294@donga.edu.vn>
2026-03-19 02:28:50 +0700

    arrow

---
+++ Assets/Script/player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 8f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float moveX;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+
+        // nhảy
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        // lật nhân vật
+        if (moveX > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveX < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // animation
+        anim.SetFloat("speed", Mathf.Abs(moveX));
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
